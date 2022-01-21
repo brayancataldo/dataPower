78147c4c4faa9fd0a6509b69644a394268607331
Brayan Cataldo <81030838+brayancataldo@users.noreply.github.com>
2022-01-21 20:57:24 -0300

    aa

--- server.js
+++ server.js
@@ -1,67 +1,26 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const nodemailer = require('nodemailer');
+
+const { resolve } = require('path');
+
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use('/', express.static(
+    resolve(__dirname, './build')
+))
 
-app.post('/api/form', (req, res) => {
+app.listen(process.env.PORT || 3000, (err) => {
+    if(err){
+        return console.log(err)
+    }
+    console.log('Tudo funcionando certinho')
+})
 
-    nodemailer.createTestAccount((err, account) => {
-        const htmlEmail = `
-        <h3>Contact Details</h3>
-        <ul>
-        <li>Name: ${req.body.name}</li>
-        <li>Email: ${req.body.email}</li>
-        </ul>
-        <h3>Message</h3>
-        <p>${req.body.message}</p>
-        `;
+   if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
 
-        let transporter = nodemailer.createTransport({
-            host: 'smtp.ethereal.email',
-            port: 587,
-            auth: {
-                user: 'config',
-                pass: 'config'
-            }
-        });
-
-        let mailOptions = {
-            from: 'config',
-            to: 'config',
-            replyTo: 'config',
-            subject: 'New message',
-            text: req.body.message,
-            html: htmlEmail
-        };
-
-        transporter.sendMail(mailOptions, (err, info) => {
-            if(err){
-                return console.log(err);
-            }
-
-            console.log('Message sent: %s', info.message);
-            console.log('Message URL: %s', nodemailer.getTestMessageUrl(info));
-        });
-    });
-
-});
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-    console.log('Server listening on port ' + PORT);
-});
-
-if (process.env.NODE_ENV === 'production') {
-    // Exprees will serve up production assets
-    app.use(express.static('client/build'));
-  
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+  // Express serve up index.html file if it doesn't recognize route
+  app.get('*', (req, res) => {
+    res.sendFile(resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
